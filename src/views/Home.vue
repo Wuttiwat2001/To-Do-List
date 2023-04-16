dae4709f4d5d37db5e43b1f4e3db393bed2bae03
Wuttiwat2001 <captenlnw_za@hotmail.com>
2023-04-16 21:38:47 +0700

    Sub Component TodoList & Props Data Sharing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,34 +14,18 @@
           <v-btn block color="success" @click="addTask">Add</v-btn>
         </v-col>
       </v-row>
-      <v-card>
-        <v-card-title primary-title>
-          <v-container>
-            <v-row
-              class="d-flex flex-row"
-              v-for="(item, index) in todos"
-              :key="index"
-            >
-              <v-col cols="12" class="d-flex flex-row align-center">
-                <v-checkbox v-model="value" value="value"></v-checkbox>
-                <span>{{ item.title }}</span>
-                <v-spacer></v-spacer>
-                <v-btn color="error">X</v-btn>
-              </v-col>
-              <v-col cols="12">
-                <v-divider></v-divider>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-title>
-      </v-card>
+      <TodoList :todos="todos"/>
     </v-container>
   </div>
 </template>
 
 <script>
+import TodoList from "@/components/TodoList";
 export default {
   name: "Home",
+  components: {
+    TodoList,
+  },
   data() {
     return {
       taskTextField: "",
@@ -61,7 +45,7 @@ export default {
         title: this.taskTextField,
         completed: false,
       });
-      this.taskTextField = ""
+      this.taskTextField = "";
     },
   },
 };
