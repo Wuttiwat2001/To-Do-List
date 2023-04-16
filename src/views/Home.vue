7b9b3cae40f522b10a8edb23214211940ac143d7
Wuttiwat2001 <captenlnw_za@hotmail.com>
2023-04-16 20:51:50 +0700

    just fix layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="home">
     <v-container>
-      <v-row
-        class="d-flex flex-row align-center"
-        v-for="(item, index) in todos"
-        :key="index"
-      >
-        <v-checkbox v-model="value" value="value"></v-checkbox>
-        <span>{{ item.title }}</span>
-        <v-spacer></v-spacer>
-        <v-btn color="error">X</v-btn>
+      <v-row>
+        <v-col cols="10">
+          <v-text-field
+          name="name"
+          label="label"
+          id="id"
+        ></v-text-field>
+        </v-col>
+        <v-col cols="2">
+          <v-btn block color="success">Add</v-btn>
+        </v-col>
       </v-row>
+      <v-card>
+        <v-card-title primary-title>
+          <v-container>
+            <v-row
+              class="d-flex flex-row"
+              v-for="(item, index) in todos"
+              :key="index"
+            >
+            <v-col cols="12" class="d-flex flex-row align-center"> 
+              <v-checkbox v-model="value" value="value"></v-checkbox>
+              <span>{{ item.title }}</span>
+              <v-spacer></v-spacer>
+              <v-btn color="error">X</v-btn>
+            </v-col>
+            <v-col cols="12">
+              <v-divider></v-divider>
+            </v-col>
+            </v-row>
+          </v-container>
+        </v-card-title>
+      </v-card>
     </v-container>
   </div>
 </template>
