d9a35ef52ed22677ca37dde008b5aacbec272dea
Wuttiwat2001 <captenlnw_za@hotmail.com>
2023-04-16 21:30:25 +0700

    add methods addTask push in todos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,14 @@
       <v-row>
         <v-col cols="10">
           <v-text-field
-          name="name"
-          label="label"
-          id="id"
-        ></v-text-field>
+            name="name"
+            label="label"
+            id="id"
+            v-model="taskTextField"
+          ></v-text-field>
         </v-col>
         <v-col cols="2">
-          <v-btn block color="success">Add</v-btn>
+          <v-btn block color="success" @click="addTask">Add</v-btn>
         </v-col>
       </v-row>
       <v-card>
@@ -21,15 +22,15 @@
               v-for="(item, index) in todos"
               :key="index"
             >
-            <v-col cols="12" class="d-flex flex-row align-center"> 
-              <v-checkbox v-model="value" value="value"></v-checkbox>
-              <span>{{ item.title }}</span>
-              <v-spacer></v-spacer>
-              <v-btn color="error">X</v-btn>
-            </v-col>
-            <v-col cols="12">
-              <v-divider></v-divider>
-            </v-col>
+              <v-col cols="12" class="d-flex flex-row align-center">
+                <v-checkbox v-model="value" value="value"></v-checkbox>
+                <span>{{ item.title }}</span>
+                <v-spacer></v-spacer>
+                <v-btn color="error">X</v-btn>
+              </v-col>
+              <v-col cols="12">
+                <v-divider></v-divider>
+              </v-col>
             </v-row>
           </v-container>
         </v-card-title>
@@ -43,6 +44,7 @@ export default {
   name: "Home",
   data() {
     return {
+      taskTextField: "",
       todos: [
         { id: 1, title: "Task 1", completed: false },
         { id: 2, title: "Task 2", completed: true },
@@ -50,6 +52,17 @@ export default {
         { id: 4, title: "Task 4", completed: false },
       ],
     };
+  },
+  methods: {
+    addTask() {
+      const id = this.todos.length + 1;
+      this.todos.push({
+        id: id,
+        title: this.taskTextField,
+        completed: false,
+      });
+      this.taskTextField = ""
+    },
   },
 };
 </script>
