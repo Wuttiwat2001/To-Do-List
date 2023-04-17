fd4e0cbf2245b0477c342894e5d89b4dbaee604e
Wuttiwat2001 <captenlnw_za@hotmail.com>
2023-04-17 16:44:20 +0700

    sup components appbar

---
+++ src/components/Appbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="../assets/to-do-list.png"
+        transition="scale-transition"
+        width="40"
+      />
+      <h3>
+        TO-DO-LIST
+      </h3>
+    </div>
+  </v-app-bar>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Appbar'
+}
+</script>
+<style>
+  
+</style>
